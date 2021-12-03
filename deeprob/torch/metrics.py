fix FID score computation

deeprob/torch/metrics.py
@@ -1,11 +1,13 @@
 from typing import Optional, Union, Any
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn
 from torch.utils import data
 from torchvision import transforms
 from torchvision import models
+from tqdm import tqdm
 
 from deeprob.utils.statistics import compute_fid
 
@@ -52,9 +54,10 @@ def fid_score(
     dataset2: Union[data.Dataset, torch.Tensor],
     model: Optional[nn.Module] = None,
     transform: Optional[Any] = None,
-    batch_size: int = 128,
+    batch_size: int = 100,
     num_workers: int = 0,
-    device: Optional[torch.device] = None
+    device: Optional[torch.device] = None,
+    verbose: bool = True
 ) -> float:
     """
     Compute the Frechet Inception Distance (FID) between two data samples.
@@ -70,11 +73,12 @@ def fid_score(
     :param batch_size: The batch size to use when extracting features.
     :param num_workers: The number of workers used for the data loaders.
     :param device: The device used to run the model. If it's None 'cuda' will be used, if available.
+    :param verbose: Whether to enable verbose mode.
     :return The FID score.
     """
     if model is None:
         # Load the InceptionV3 model pretrained on ImageNet
-        model = models.inception_v3(pretrained=True, aux_logits=False)
+        model = models.inception_v3(pretrained=True, aux_logits=False, transform_input=False)
 
         # Remove dropout and fully-connected layers (we are interested in extracted features)
         model.dropout = nn.Identity()
@@ -89,29 +93,29 @@ def fid_score(
 
     # Extract the features of the two data sets
     features1 = extract_features(
-        model, dataset1, transform,
-        device=device, batch_size=batch_size, num_workers=num_workers
+        model, dataset1, transform, device=device, verbose=verbose,
+        batch_size=batch_size, num_workers=num_workers
     )
     features2 = extract_features(
-        model, dataset2, transform,
-        device=device, batch_size=batch_size, num_workers=num_workers
+        model, dataset2, transform, device=device, verbose=verbose,
+        batch_size=batch_size, num_workers=num_workers
     )
 
     # Compute the statistics (mean and covariance of the features)
-    mean1, cov1 = torch.mean(features1, dim=0), torch.cov(features1.T)
-    mean2, cov2 = torch.mean(features2, dim=0), torch.cov(features2.T)
+    features1, features2 = features1.cpu().numpy(), features2.cpu().numpy()
+    mean1, cov1 = np.mean(features1, axis=0), np.cov(features1, rowvar=False)
+    mean2, cov2 = np.mean(features2, axis=0), np.cov(features2, rowvar=False)
 
     # Compute and return the FID score
-    mean1, cov1 = mean1.cpu().numpy(), cov1.cpu().numpy()
-    mean2, cov2 = mean2.cpu().numpy(), cov2.cpu().numpy()
     return compute_fid(mean1, cov1, mean2, cov2)
 
 
 def extract_features(
     model: nn.Module,
-    dataset: data.Dataset,
+    dataset: Union[data.Dataset, torch.Tensor],
     transform: Optional[Any] = None,
     device: Optional[torch.device] = None,
+    verbose: bool = True,
     **kwargs
 ) -> torch.Tensor:
     """
@@ -121,6 +125,7 @@ def extract_features(
     :param dataset: The data set.
     :param transform: An optional transformation to apply to every sample.
     :param device: The device used to run the model. If it's None 'cuda' will be used, if available.
+    :param verbose: Whether to enable verbose mode.
     :param kwargs: Additional parameters to pass to the data loader.
     :return: The extracted features for each data sample.
     """
@@ -131,6 +136,10 @@ def extract_features(
 
     # Instantiate the data loader
     data_loader = data.DataLoader(dataset, **kwargs)
+    if verbose:
+        data_loader = tqdm(
+            data_loader, leave=False, bar_format='{l_bar}{bar:24}{r_bar}', unit='batch'
+        )
 
     # Make sure the model is in evaluation mode
     # Moreover, move it to the desired device

deeprob/utils/statistics.py
@@ -136,7 +136,8 @@ def compute_fid(
     cov1: np.ndarray,
     mean2: np.ndarray,
     cov2: np.ndarray,
-    blocksize: int = 64
+    blocksize: int = 64,
+    eps: float = 1e-6
 ) -> float:
     """
     Computes the Frechet Inception Distance (FID) between two multivariate Gaussian distributions.
@@ -147,6 +148,7 @@ def compute_fid(
     :param mean2: The mean of the second multivariate Gaussian.
     :param cov2: The covariance of the second multivariate Gaussian.
     :param blocksize: The block size used by the matrix square root algorithm.
+    :param eps: Epsilon value used to avoid singular matrices.
     :return: The FID score.
     :raises ValueError: If there is a shape mismatch between input arrays.
     """
@@ -161,7 +163,7 @@ def compute_fid(
 
     # Compute the matrix square root of the dot product between covariance matrices
     epsdiag = np.zeros_like(cov1)
-    np.fill_diagonal(epsdiag, np.finfo(np.float32).eps)
+    np.fill_diagonal(epsdiag, eps)
     sqrtcov, _ = linalg.sqrtm(np.dot(cov1 + epsdiag, cov2 + epsdiag), disp=False, blocksize=blocksize)
 
     # Numerical errors might give a complex output, even if the input arrays are real

experiments/flows.py
@@ -8,7 +8,6 @@ from deeprob.flows.models.maf import MAF
 from deeprob.flows.models.realnvp import RealNVP1d, RealNVP2d
 from deeprob.utils.statistics import compute_bpp
 from deeprob.torch.metrics import fid_score
-from deeprob.torch.transforms import Quantize
 
 from experiments.datasets import load_continuous_dataset, load_vision_dataset
 from experiments.datasets import CONTINUOUS_DATASETS, VISION_DATASETS
@@ -181,7 +180,7 @@ if __name__ == '__main__':
     if is_vision_dataset:
         bpp = compute_bpp(mean_ll, data_train.features_shape)
         if args.model in ['realnvp-2d']:
-            samples = collect_samples(model, n_samples=5000, batch_size=args.batch_size)
+            samples = collect_samples(model, 5000, batch_size=args.batch_size)
             del model  # Delete the model to reserve some extra memory required to compute the FID score
             fid = fid_score(data_test, samples, batch_size=max(1, args.batch_size // 4))
 